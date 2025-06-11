112d423b354cf49c5b60a1c86c8106c8d0912f9e
carlitosxm <carlos_xavi@live.com>
2025-06-10 23:12:58 -0500

    multiplicar

---
+++ Clases/src/com/krakedev/Mensajeria.java
@@ -0,0 +1,10 @@
+package com.krakedev;
+
+public class Mensajeria {
+	public void saludar(){// no retorna nada
+	System.out.println("Hola HUMANOS");
+}
+	public void saludarAmigo(String nombre, String apellido) {
+		System.out.println("Hola "+nombre+" "+apellido);
+	}
+}
---
+++ Clases/src/com/krakedev/TestMensaje.java
@@ -0,0 +1,9 @@
+package com.krakedev;
+
+public class TestMensaje {
+	public static void main(String[] args) {
+		Mensajeria m=new Mensajeria();
+		m.saludar();
+		m.saludarAmigo("Pepe", "Chavez");
+	}
+}
