a484d3ca8bb1fa9622af92722f606912c528519d
carlitosxm <carlos_xavi@live.com>
2025-07-27 22:43:31 -0500

    Reto 13

--- inventarios/src/main/java/com/krakedev/inventarios/bdd/PedidosBDD.java
+++ inventarios/src/main/java/com/krakedev/inventarios/bdd/PedidosBDD.java
@@ -1,10 +1,10 @@
 package com.krakedev.inventarios.bdd;
 
-import java.sql.Statement;
 import java.math.BigDecimal;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.Date;
 
@@ -17,7 +17,7 @@ public class PedidosBDD {
 	public void insertar(Pedido pedido) throws KrakeDevException {
 		Connection con = null;
 		PreparedStatement ps = null;
-		PreparedStatement psDet=null;
+		PreparedStatement psDet = null;
 		ResultSet rsClave = null;
 		int codigoCabecera = 0;
 		try {
@@ -35,25 +35,26 @@ public class PedidosBDD {
 			if (rsClave.next()) {
 				codigoCabecera = rsClave.getInt(1);
 			}
-			
-			ArrayList<DetallePedido> detallesPedido= pedido.getDetalles();
+
+			ArrayList<DetallePedido> detallesPedido = pedido.getDetalles();
 			DetallePedido det;
-			for(int i=0;i<detallesPedido.size();i++) {
+			for (int i = 0; i < detallesPedido.size(); i++) {
 				det = detallesPedido.get(i);
-				psDet=con.prepareStatement("INSERT INTO detalle_pedidos "
+				psDet = con.prepareStatement("INSERT INTO detalle_pedidos "
 						+ "(id_cabecera_pedido, id_producto, cantidad, subtotal, cantidad_recibida) VALUES "
 						+ "( ?, ?, ?, ?, ?);");
 				psDet.setInt(1, codigoCabecera);
 				psDet.setInt(2, det.getProducto().getCodigo());
 				psDet.setInt(3, det.getCantidadSolicitada());
-				psDet.setInt(4, 0);
-				BigDecimal pv=det.getProducto().getPrecioVenta();
+
+				BigDecimal pv = det.getProducto().getPrecioVenta();
 				BigDecimal cantidad = new BigDecimal(det.getCantidadSolicitada());
-				BigDecimal subtoral=pv.multiply(cantidad);
-				psDet.setBigDecimal(5, subtoral);
-				
+				BigDecimal subtoral = pv.multiply(cantidad);
+				psDet.setBigDecimal(4, subtoral);
+				psDet.setInt(5, 0);
+
 				psDet.executeUpdate();
-				
+
 			}
 
 		} catch (KrakeDevException e) {
@@ -64,4 +65,51 @@ public class PedidosBDD {
 			throw new KrakeDevException("Error al insertar productos" + e.getMessage());
 		}
 	}
+
+	public void recibir(Pedido pedido) throws KrakeDevException {
+		Connection con = null;
+		PreparedStatement ps = null;
+		PreparedStatement psDec = null;
+		try {
+			con = ConexionBD.obtenerConexion();
+			ps = con.prepareStatement(" update cabecera_pedidos set id_estado='R' where numero = ? ; ");
+			ps.setInt(1, pedido.getNumero());
+			
+			System.out.println(pedido.getNumero());
+
+			int filasAfectadas = ps.executeUpdate();
+
+			if (filasAfectadas < 1) {
+				throw new KrakeDevException("No se encontró el pedido con el número especificado.");
+			}
+
+			psDec = con.prepareStatement(" update detalle_pedidos set cantidad_recibida = ? , subtotal = ? where codigo = ? ;");
+			ArrayList<DetallePedido> detallesPedido = pedido.getDetalles();
+			DetallePedido dep;
+			for (int i = 0; i < detallesPedido.size(); i++) {
+				dep = detallesPedido.get(i);
+				psDec.setInt(1, dep.getCantidadRecibida());
+				BigDecimal pv = dep.getProducto().getPrecioVenta();
+				BigDecimal cantidad = new BigDecimal(dep.getCantidadRecibida());
+				BigDecimal subtotal = pv.multiply(cantidad);
+				psDec.setBigDecimal(2, subtotal);
+				psDec.setInt(3, dep.getCodigo());
+
+				int filasAfectadas2 = psDec.executeUpdate();
+
+				if (filasAfectadas2 < 1) {
+					throw new KrakeDevException("No se encontró el detalle de pedido con el número especificado.");
+				}
+
+			}
+
+		} catch (KrakeDevException e) {
+			e.printStackTrace();
+			throw e;
+		} catch (Exception e) {
+			e.printStackTrace();
+			throw new KrakeDevException("Error al insertar productos" + e.getMessage());
+		}
+	}
+
 }
--- inventarios/src/main/java/com/krakedev/inventarios/servicios/ServicioPedido.java
+++ inventarios/src/main/java/com/krakedev/inventarios/servicios/ServicioPedido.java
@@ -2,6 +2,7 @@ package com.krakedev.inventarios.servicios;
 
 import javax.ws.rs.Consumes;
 import javax.ws.rs.POST;
+import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
@@ -21,6 +22,22 @@ public class ServicioPedido {
 		PedidosBDD pedid=new PedidosBDD();
 		try {
 			pedid.insertar(pedido);
+			return Response.ok().build();
+		} catch (KrakeDevException e) {
+			e.printStackTrace();
+			return Response.serverError().build();
+		}
+	}
+	
+	
+	@Path("recibir")
+	@PUT
+	@Consumes(MediaType.APPLICATION_JSON)
+	public Response recibir(Pedido pedido) {
+		System.out.println(">>>>>>>>>>>>"+pedido);
+		PedidosBDD pedid=new PedidosBDD();
+		try {
+			pedid.recibir(pedido);
 			return Response.ok().build();
 		} catch (KrakeDevException e) {
 			e.printStackTrace();
